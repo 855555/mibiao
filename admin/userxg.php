4fe05199ce4e1d5bf70f63eeec3c3d74e122c56f
TsaoYan <42229298+TsaoYan@users.noreply.github.com>
2023-07-31 23:59:40 +0800

    Update userxg.php

--- admin/userxg.php
+++ admin/userxg.php
@@ -1,5 +1,6 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . '/sjk/sjk.php');  
+include($_SERVER['DOCUMENT_ROOT'] . '/sjk/sjk.php');
+
 $id = 1;
 $zhanghao = $_POST['zhanghao'];
 $password = $_POST['password'];
@@ -10,19 +11,28 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
 // 判断是否有实际修改
-if ($row['zhanghao'] == $zhanghao && password_verify($password, $row['password'])) {
+$isModified = false;
+$updateFields = array();
+
+if ($row['zhanghao'] != $zhanghao) {
+    $isModified = true;
+    $updateFields[] = "zhanghao = '$zhanghao'";
+}
+
+if ($row['password'] != $password) {
+    $isModified = true;
+    $updateFields[] = "password = '$password'";
+}
+
+// 如果没有实际修改，则不执行更新操作
+if (!$isModified) {
     echo '<script>alert("没有进行任何修改"); window.location.href = "index.php";</script>';
     exit;
 }
 
-// 哈希加密密码
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-// 更新数据
-$sql = "UPDATE user SET
-        zhanghao = '$zhanghao',
-        password = '$hashedPassword'
-        WHERE id = $id";
+// 构建更新数据的 SQL 语句
+$updateFieldsStr = implode(", ", $updateFields);
+$sql = "UPDATE user SET $updateFieldsStr WHERE id = $id";
 
 $rs = mysqli_query($conn, $sql);
 
